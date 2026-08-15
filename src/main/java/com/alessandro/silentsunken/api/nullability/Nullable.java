a42c7c8ee852689545867b5ccfca4bb688cbbe65
alessandro <alessandro.casale.developer@gmail.com>
2026-08-15 14:13:14 +0200

    api: add nullability annotations

---
+++ src/main/java/com/alessandro/silentsunken/api/nullability/Nullable.java
@@ -0,0 +1,12 @@
+package com.alessandro.silentsunken.api.nullability;
+
+import javax.annotation.Nonnull;
+import javax.annotation.meta.TypeQualifierNickname;
+import javax.annotation.meta.When;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+@TypeQualifierNickname
+@Nonnull(when = When.UNKNOWN)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Nullable { }
